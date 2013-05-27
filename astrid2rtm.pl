15382a346fc2e534023aed19395ff114783175d5
Randy Stauner <randy@magnificent-tears.com>
2013-05-27 10:46:08 -0700

    Prototype astrid xml conversion to rtm smart add
    unfortunately smart add doesn't allow me to preserve notes

---
+++ astrid2rtm.pl
@@ -0,0 +1,126 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Moo;
+use MooX::Types::MooseLike::Base qw( ArrayRef );
+use XML::Simple;
+use Time::Stamp 'localstamp';
+# TODO: use Getopt::Long qw( GetOptions );
+
+has xml_parser => (
+  is      => 'lazy',
+  builder => 1,
+);
+
+has default_tags => (
+  is      => 'ro',
+  isa     => ArrayRef,
+  default => sub { ['astrid'] },
+);
+
+sub _build_xml_parser {
+  XML::Simple->new(
+    ForceArray => [qw(
+      task
+      metadata
+      tags-tag
+    )],
+  #KeyAttr => { metadata => 'key' },
+  #ValueAttr => { 'tags-tag' => 'value' },
+  );
+}
+
+sub parse_xml {
+  my ($self, $xml) = @_;
+  $self->xml_parser->xml_in($xml);
+}
+
+sub run {
+  my ($self, $in) = @_;
+  my $astrid = $self->parse_xml($in);
+
+  die 'Only known to work with dump format == 2'
+    if $astrid->{format} != 2;
+
+  print sort map { $self->smart_add($_) } @{ $astrid->{task} };
+
+}
+
+sub format_due_date {
+  my ($self, $date) = @_;
+  return unless $date;
+  $date =~ s/000$//
+    or die "Date '$date' in unknown format";
+  return localstamp($date);
+}
+
+sub priority {
+  my ($self, $priority) = @_;
+  # off by one error ;-)
+  return $priority + 1;
+}
+
+my %freq = (
+  DAILY   => 'days',
+  MONTHLY => 'months',
+  WEEKLY  => 'weeks',
+  YEARLY  => 'years',
+);
+my %day  = (
+  SU => 'Sunday',
+);
+
+sub format_repeat {
+  my ($self, $repeat) = @_;
+  return unless $repeat;
+
+  die 'Unknown recurrence format'
+    unless $repeat =~ s/^RRULE:((?:[A-Z]+=[^;]+;?)+)$/$1/;
+
+  my %attr = map { split /=/ } split /;/, $repeat;
+
+  return join(' ', grep { $_ }
+    (
+      $attr{INTERVAL}
+        ? ( every => $attr{INTERVAL}, $freq{ $attr{FREQ} } )
+        : lc($attr{FREQ})
+    ),
+    ($attr{BYDAY} ? (on => $day{ $attr{BYDAY} } || $attr{BYDAY}) : ()),
+  );
+}
+
+sub smart_add {
+  my ($self, $task) = @_;
+  return join(' ', grep { $_ }
+    $task->{title},
+    $self->format_tags($task),
+    $self->smart_part('!', $self->priority($task->{importance})),
+    $self->smart_part('^', $self->format_due_date($task->{dueDate})),
+    $self->smart_part('*', $self->format_repeat($task->{recurrence})),
+  ) . "\n";
+}
+
+sub smart_part {
+  my ($self, $prefix, $value) = @_;
+  return $value ? $prefix . $value : '';
+}
+
+sub format_tags {
+  my ($self, $task) = @_;
+
+  my $tags = $task->{metadata}{'tags-tag'} || [];
+  $tags = [$tags] unless ref($tags) eq 'ARRAY';
+
+  return ( map { '#' . $_ }
+    @{ $self->default_tags },
+
+    # tag these so they can easily be found and fixed in the web interface
+    ($task->{completed} ? 'astrid-completed' : ()),
+    ($task->{notes}     ? 'astrid-notes'     : ()),
+
+    map { $_->{value} } @$tags,
+  );
+}
+
+__PACKAGE__->new->run(@ARGV);
