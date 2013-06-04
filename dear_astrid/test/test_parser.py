a455c01b270fcbd13ea623f2a7ba37c8af6e5b5a
Randy Stauner <randy@magnificent-tears.com>
2013-06-03 18:14:14 -0700

    Test additional tasks to cover more attributes

--- dear_astrid/test/test_parser.py
+++ dear_astrid/test/test_parser.py
@@ -62,6 +62,74 @@ def test_parse_xml():
     }
   )
 
+  assert_equal(
+    one_task(
+      '''
+  <task attachments_pushed_at="0" calendarUri="" classification="" completed="1370214214527" created="1370214183094" creatorId="0" deleted="0" detailsDate="0" dueDate="0" elapsedSeconds="0" estimatedSeconds="0" flags="0" hideUntil="0" historyFetch="0" historyHasMore="0" importance="3" is_public="0" is_readonly="0" lastSync="0" modified="1370214214638" notes="" postponeCount="0" pushedAt="0" recurrence="" notificationFlags="6" lastNotified="0" notifications="0" snoozeTime="0" repeatUntil="0" socialReminder="unseen" timerStart="0" title="Completed no priority" user="" activities_pushed_at="0" userId="0" remoteId="4514989953482146569" />
+      '''
+    ),
+    {
+      'title':        u'Completed no priority',
+      'priority':     3,
+      'due_date':     None,
+      'recurrence':   None,
+      'repeat_until': None,
+      'completed':    datetime(2013,  6,  2, 16,  3, 34, 527000),
+      'deleted':      None,
+      'estimated':    0,
+      'elapsed':      0,
+      'notes':        None,
+      'tags':         ['astrid'],
+    }
+  )
+
+  assert_equal(
+    one_task(
+      '''
+  <task attachments_pushed_at="0" calendarUri="" classification="" completed="0" created="1370214247839" creatorId="0" deleted="0" detailsDate="0" dueDate="0" elapsedSeconds="0" estimatedSeconds="0" flags="0" hideUntil="0" historyFetch="0" historyHasMore="0" importance="0" is_public="0" is_readonly="0" lastSync="0" modified="1370234693330" notes="No, really" postponeCount="0" pushedAt="0" recurrence="" notificationFlags="6" lastNotified="0" notifications="0" snoozeTime="0" repeatUntil="0" socialReminder="unseen" timerStart="0" title="Really important" user="" activities_pushed_at="0" userId="0" remoteId="1480747625840088568">
+    <metadata created="0" deleted="0" key="tags-tag" value="section 8" value2="2153874380669753982" value3="1480747625840088568" />
+    <metadata created="0" deleted="0" key="tags-tag" value="nifty" value2="654272611227692712" value3="1480747625840088568" />
+  </task>
+      '''
+    ),
+    {
+      'title':        u'Really important',
+      'priority':     0,
+      'due_date':     None,
+      'recurrence':   None,
+      'repeat_until': None,
+      'completed':    None,
+      'deleted':      None,
+      'estimated':    0,
+      'elapsed':      0,
+      'notes':        u'No, really',
+      'tags':         ['astrid', u'section 8', 'nifty'],
+    }
+  )
+
+  assert_equal(
+    one_task(
+      '''
+  <task attachments_pushed_at="0" calendarUri="" classification="" completed="0" created="1370214439149" creatorId="0" deleted="0" detailsDate="0" dueDate="0" elapsedSeconds="2100" estimatedSeconds="8100" flags="0" hideUntil="0" historyFetch="0" historyHasMore="0" importance="1" is_public="0" is_readonly="0" lastSync="0" modified="1370234827340" notes="" postponeCount="0" pushedAt="0" recurrence="RRULE:FREQ=WEEKLY;INTERVAL=3;BYDAY=TH" notificationFlags="6" lastNotified="0" notifications="0" snoozeTime="0" repeatUntil="0" socialReminder="unseen" timerStart="0" title="Funky ch&amp;rs !n ^title a =b" user="" activities_pushed_at="0" userId="0" remoteId="583744141993888906">
+    <metadata created="0" deleted="0" key="tags-tag" value="Hard cheese" value2="799352962683373419" value3="583744141993888906" />
+  </task>
+      '''
+    ),
+    {
+      'title':        u'Funky ch&rs !n ^title a =b',
+      'priority':     1,
+      'due_date':     None,
+      'recurrence':   dict(FREQ=u'WEEKLY', INTERVAL=3, BYDAY=u'TH'),
+      'repeat_until': None,
+      'completed':    None,
+      'deleted':      None,
+      'estimated':    8100,
+      'elapsed':      2100,
+      'notes':        None,
+      'tags':         ['astrid', 'Hard cheese'],
+    }
+  )
+
 
 def test_parse_timestamp():
   def t(stamp, exp):
