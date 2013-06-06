40da3ddcd3495cf548f23430b546cc2bb390806c
Randy Stauner <randy@magnificent-tears.com>
2013-06-06 07:33:46 -0700

    Define constants for mapping values

---
+++ dear_astrid/constants.py
@@ -0,0 +1,29 @@
+"""Constants for Astrid task attributes"""
+
+# TODO: make these all lists?
+FREQUENCY_UNITS = {
+  'DAILY':   'day',
+  'MONTHLY': 'month',
+  'WEEKLY':  'week',
+  'YEARLY':  'year',
+}
+
+RRULE_WEEK_DAYS = {
+  'SU': 0,
+  'MO': 1,
+  'TU': 2,
+  'WE': 3,
+  'TH': 4,
+  'FR': 5,
+  'SA': 6,
+}
+
+WEEK_DAY_NAMES = (
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+)
