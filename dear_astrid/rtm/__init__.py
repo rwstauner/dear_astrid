6191868fc47fa263c19860e96b29439cdbc0e504
Randy Stauner <randy@magnificent-tears.com>
2013-07-03 20:08:20 -0700

    Move rtm format functions to a sub-module
    to make room for importer module

---
+++ dear_astrid/rtm/__init__.py
@@ -0,0 +1,10 @@
+"""
+Convert Astrid tasks to Remember The Milk.
+"""
+
+from __future__ import absolute_import
+
+from dear_astrid.rtm.format import *
+from dear_astrid.rtm.format import __all__ as _format_all
+
+__all__ = _format_all
