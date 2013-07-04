33594dd53ffab2898f57090c1566e69ce891f1a1
Randy Stauner <randy@magnificent-tears.com>
2013-07-03 23:01:00 -0700

    Use Mock to test that rtm sleeps before delegating

---
+++ dear_astrid/test/test_rtm_importer.py
@@ -0,0 +1,39 @@
+# pylint: disable=wildcard-import,unused-wildcard-import,missing-docstring
+
+from __future__ import absolute_import
+
+from unittest import TestCase
+
+from nose.tools import *
+from mock import *
+
+from dear_astrid.rtm.importer import Importer as rtmimp
+
+class TestRTMImport(TestCase):
+  def setUp(self):
+    self.patches = dict(
+      time = patch('time.sleep'),
+      rtm  = patch('rtm.createRTM'),
+    )
+    self.mocks = dict()
+    for (k, v) in self.patches.iteritems():
+      self.mocks[k] = v.start()
+
+  def test_sleep_before_rtm(self):
+    imp = rtmimp(['task'])
+    imp._rtm = Mock()
+
+    assert not self.mocks['time'].called
+
+    # assert that it is our mock object
+    assert_equal(imp.rtm, imp._rtm)
+
+    self.mocks['time'].assert_called_once_with(1)
+
+    # test calling other methods
+    imp.rtm.foo.bar
+
+    self.mocks['time'].assert_has_calls([ call(1), call(1) ])
+
+    # not used this time
+    assert not self.mocks['rtm'].called
