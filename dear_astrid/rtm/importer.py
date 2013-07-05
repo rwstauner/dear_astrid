ea6007f7afb084c80de3dad56dd90bd9aa800c76
Randy Stauner <randy@magnificent-tears.com>
2013-07-04 22:39:23 -0700

    Create CLI Authorizer

--- dear_astrid/rtm/importer.py
+++ dear_astrid/rtm/importer.py
@@ -13,6 +13,7 @@ import sys
 if sys.version_info <= (3,):
   import string
 import time
+import webbrowser
 
 import rtm
 
@@ -110,6 +111,10 @@ class Importer(object):
   key    = _rot('key')
   secret = _rot('secret')
 
+RTMAPIError = rtm.rtm.RTMAPIError
+class AuthError(RTMAPIError):
+  pass
+
 class BaseAuth(object):
   def __init__(self, key, secret, token=None):
     self.key    = key
@@ -127,3 +132,37 @@ class BaseAuth(object):
 
   def auth(self):
     return self.api(self.token)
+
+class CLIAuth(BaseAuth):
+  def prompt(self, text):
+    try:
+      answer = raw_input(text)
+    except NameError:
+      answer = input(text)
+    return answer
+
+  def message(self, *args):
+    for arg in args:
+      print(arg)
+
+  def get_token_from_api(self):
+    # Instantiate api with fake token to get url.
+    api = self.api('dear_astrid', test_login=False)
+    url = api.getAuthURL()
+
+    webbrowser.open(url, True, True)
+    self.message(
+      "You must authorize this app with RTM.",
+      "If a browser does not open visit this URL:",
+      url,
+    )
+    self.prompt('Press Enter after you have authorized this app through RTM.')
+
+    return api.getToken()
+
+  def auth(self):
+    token = self.get_token_from_api()
+    try:
+      return self.api(token)
+    except RTMAPIError:
+      raise AuthError("Failed to authorize app with RTM.")
