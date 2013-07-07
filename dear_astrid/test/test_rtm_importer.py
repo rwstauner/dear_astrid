a7d54ef8ee86919fc2438ab09fd91084f1d4b0e2
Randy Stauner <randy@magnificent-tears.com>
2013-07-06 22:11:00 -0700

    Rewrite tests to use patch decorator on class
    rather than setup/start/teardown/stop.
    
    Not only because I failed to do the tearDown part,
    but it's less error-prone (obviously),
    and just plain simpler.

--- dear_astrid/test/test_rtm_importer.py
+++ dear_astrid/test/test_rtm_importer.py
@@ -15,40 +15,34 @@ try:
   INPUT_FUNC = '__builtin__.raw_input'
 except NameError:
   INPUT_FUNC = 'builtins.input'
+INPUT_MOCK = MagicMock()
 
+@patch('time.sleep')
+@patch('rtm.createRTM')
+@patch('webbrowser.open')
+@patch(INPUT_FUNC, new=INPUT_MOCK)
 class TestRTMImport(TestCase):
-  def setUp(self):
-    self.patches = dict(
-      time = patch('time.sleep'),
-      rtm  = patch('rtm.createRTM'),
-      browser = patch('webbrowser.open'),
-      prompt  = patch(INPUT_FUNC)
-    )
 
-    self.mocks = dict()
-    for (k, v) in self.patches.items():
-      self.mocks[k] = v.start()
-
-  def test_sleep_before_rtm(self):
+  def test_sleep_before_rtm(self, *args):
     imp = Importer(['task'])
     imp._rtm = Mock()
 
-    assert not self.mocks['time'].called
+    assert not time.sleep.called
 
     # Assert that it is in fact our mock object.
     assert_equal(imp.rtm, imp._rtm)
 
-    self.mocks['time'].assert_called_once_with(1)
+    time.sleep.assert_called_once_with(1)
 
     # Test chaining method calls.
     imp.rtm.foo.bar
 
-    self.mocks['time'].assert_has_calls([ call(1), call(1) ])
+    time.sleep.assert_has_calls([ call(1), call(1) ])
 
     # Not used this time.
-    assert not self.mocks['rtm'].called
+    assert not rtm.createRTM.called
 
-  def test_deobfuscator(self):
+  def test_deobfuscator(self, *args):
     imp = Importer(['task'])
 
     imp.key = 'a92'
@@ -57,31 +51,31 @@ class TestRTMImport(TestCase):
     imp.secret = 'deadbeef'
     assert imp.secret == '56253667'
 
-  def test_base_auth(self):
+  def test_base_auth(self, *args):
     api = BaseAuth('k', 's', 't').auth()
-    self.mocks['rtm'].assert_called_once_with('k', 's', 't')
+    rtm.createRTM.assert_called_once_with('k', 's', 't')
     api.test.login.assert_called_once_with()
 
     # Prepare to test Exceptions raised on login test.
     login = Mock()
     login.test.login.side_effect = AuthError('oops')
-    self.mocks['rtm'].return_value = login
+    rtm.createRTM.return_value = login
 
     def reset():
-      for mock in (login, self.mocks['rtm']):
+      for mock in (login, rtm.createRTM):
         mock.reset_mock()
 
     reset()
 
     auth = BaseAuth(1, 2, 3)
     assert_raises(AuthError, auth.auth)
-    self.mocks['rtm'].assert_called_once_with(1, 2, 3)
+    rtm.createRTM.assert_called_once_with(1, 2, 3)
     login.assert_has_calls([call.test.login()])
 
     reset()
 
     assert_raises(AuthError, auth.api)
-    self.mocks['rtm'].assert_called_once_with(1, 2, 3)
+    rtm.createRTM.assert_called_once_with(1, 2, 3)
     login.assert_has_calls([call.test.login()])
 
     reset()
@@ -95,7 +89,7 @@ class TestRTMImport(TestCase):
 
     assert not login.called
 
-  def test_cli_auth(self):
+  def test_cli_auth(self, *args):
     url = 'jelly://bean'
     token = 'cookies'
 
@@ -103,14 +97,14 @@ class TestRTMImport(TestCase):
     api.getAuthURL.return_value = url
     api.getToken.return_value = token
 
-    self.mocks['rtm'].return_value = api
+    rtm.createRTM.return_value = api
 
     # Don't print during the tests
     CLIAuth.message = Mock()
 
     api = CLIAuth('K', 'S').auth()
 
-    self.mocks['rtm'].assert_has_calls([
+    rtm.createRTM.assert_has_calls([
       call('K', 'S', 'dear_astrid'),
       # no test.login()
       call().getAuthURL(),
@@ -119,13 +113,13 @@ class TestRTMImport(TestCase):
       call().test.login(),
     ])
 
-    self.mocks['browser'].called_once_with(url, True, True)
+    webbrowser.open.called_once_with(url, True, True)
 
     # Url printed to screen.
     assert CLIAuth.message.call_args[0][2] == url
 
-    assert self.mocks['prompt'].call_count == 1
-    assert self.mocks['prompt'].call_args[0][0].startswith('Press Enter')
+    assert INPUT_MOCK.call_count == 1
+    assert INPUT_MOCK.call_args[0][0].startswith('Press Enter')
 
     # We should catch RTMAPIError and throw AuthError.
     CLIAuth.get_token_from_api = Mock()
