ba8c67b3a3b92e69cffb5d11da7e45465f53d7d4
Randy Stauner <randy@magnificent-tears.com>
2013-10-16 06:50:33 -0700

    Cast unicode strings to str() for __all__ concatenation

--- dear_astrid/rtm/__init__.py
+++ dear_astrid/rtm/__init__.py
@@ -2,7 +2,9 @@
 Convert Astrid tasks to Remember The Milk.
 """
 
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
+
+# pylint: disable=wildcard-import
 
 from dear_astrid.rtm.format import *
 from dear_astrid.rtm.format import __all__ as _format_all
@@ -14,4 +16,6 @@ from dear_astrid.rtm.cli import *
 from dear_astrid.rtm.cli import __all__ as _cli_all
 
 
-__all__ = _format_all + _importer_all + _cli_all
+# With unicode_literals on everywhere I seem to get this error on py2:
+#   TypeError: Item in ``from list'' not a string
+__all__ = [str(s) for s in _format_all + _importer_all + _cli_all]
